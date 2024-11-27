11ef38549a1e6bd5ebb708450235c6de439b344b
Gabriel Alejandro Contreras Cruz <139160190+GabContreras@users.noreply.github.com>
2024-11-27 09:38:14 -0600

    Update Agalo.sql

--- Agalo.sql
+++ Agalo.sql
@@ -6,7 +6,8 @@
 //Prueba 2
 //asojdjasdsd
   //PASopsdoapcskdie
-  //Blibliblibiblib
+  //Blibliblibiblibiuosdai
+  
 CREATE TABLE DEPARTAMENTO(
 IdDepartamento INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 Nombre varchar2(50)
